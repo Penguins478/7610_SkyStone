38c064a19df87b462f52d69d26d18c49f7c64b7b
burhanr <burhan.rajgara@gmail.com>
2019-10-02 16:51:18 -0700

    servo code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModes/TestingServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModes/TestingServo.java
@@ -1,4 +1,39 @@
 package org.firstinspires.ftc.teamcode.TeleOpModes;
 
-public class TestingServo {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "servo", group = "teleop modes")
+public class TestingServo extends OpMode {
+    private Servo servol;
+    private Servo servor;
+
+    @Override
+    public void init() {
+        servol = hardwareMap.servo.get("servol");
+        servor = hardwareMap.servo.get("servor");
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void loop() {
+        boolean pressed = gamepad1.right_bumper;
+        if (pressed) {
+            servol.setPosition(0.5);
+            servor.setPosition(0.5);
+        } else {
+            servol.setPosition(0.0);
+            servor.setPosition(0.0);
+        }
+    }
+
+    @Override
+    public void stop() {
+
+    }
 }
